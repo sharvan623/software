2f297e38fa1f0f23ed3736d9332125716493f7f6
unknown <Shravan.k@VTDT025.VTECHSOLUTION.COM>
2018-05-09 10:17:45 -0400

    comment

--- gmail.php
+++ gmail.php
@@ -18,9 +18,9 @@
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          
-         $retval = mail ($to,$subject,$message,$header);
+         $retval = mail($to,$subject,$message,$header);
          
-         if( $retval == 'true' ) {
+         if( $retval == false ) {
             echo "Message sent successfully...";
          }else {
             echo "Message could not be sent...";
