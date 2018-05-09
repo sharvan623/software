9d12bcf6e5aba2a5bb5f0d0464a253399723db39
unknown <Shravan.k@VTDT025.VTECHSOLUTION.COM>
2018-05-09 10:20:15 -0400

    comment

--- gmail.php
+++ gmail.php
@@ -13,19 +13,20 @@
          $message = "<b>This is HTML message.</b>";
          $message .= "<h1>This is headline.</h1>";
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header = "From:giet13aei015@gmail.com \r\n";
+         $header .= "Cc:giet13aei016@gmail.com \r\n";
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          
          $retval = mail($to,$subject,$message,$header);
          
          if( $retval == false ) {
-            echo "Message sent successfully...";
-         }else {
             echo "Message could not be sent...";
+         }else {
+            echo "Message sent successfullyt...";
          }
       ?>
+      
       
    </body>
 </html>
