51a62d47c9b402e70a4ab624184a1c6409a7e32e
unknown <Shravan.k@VTDT025.VTECHSOLUTION.COM>
2018-05-09 10:14:31 -0400

    comment

--- gmail.php
+++ gmail.php
@@ -20,7 +20,7 @@
          
          $retval = mail ($to,$subject,$message,$header);
          
-         if( $retval == true ) {
+         if( $retval == 'true' ) {
             echo "Message sent successfully...";
          }else {
             echo "Message could not be sent...";
